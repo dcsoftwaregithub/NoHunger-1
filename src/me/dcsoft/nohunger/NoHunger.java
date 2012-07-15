83e74ebbc4648cb5d858841bcb3368c2747f535d
Joshua James <Josh@server>
2012-07-15 16:25:32 +0100

    added basic information

--- src/me/dcsoft/nohunger/NoHunger.java
+++ src/me/dcsoft/nohunger/NoHunger.java
@@ -1,14 +1,25 @@
 package me.dcsoft.nohunger;
 
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoHunger extends JavaPlugin {
+	
+	
 
 	public void onEnable() {
 		
 	}
 	
 	public void onDisable() {
+		
+	}
+	
+	private String getFormattedMessage(String msg) {
+		
+		PluginDescriptionFile pdf = this.getDescription();
+			
+		return "[" + pdf.getName() + " v" + pdf.getVersion() + "] " + msg;
 		
 	}
 }
